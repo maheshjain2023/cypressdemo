3c6c9607e1cb125c3f9f13a94c21be9db032b9f5
Mahesh <mahesh.yambal@joshsoftware.com>
2023-04-07 13:24:34 +0530

    Added Hotels function

--- cypress/e2e/Assignment2.cy.js
+++ cypress/e2e/Assignment2.cy.js
@@ -1,14 +1,34 @@
 describe('Goibibo',()=>{
-
-    it('Flight Booking',()=>{
-
+      before('Before Hook',()=>{
         cy.visit('https://www.goibibo.com/')
-        // cy.get(".sc-eLwHnm.hHxEGr.fswWidgetPlaceholder").eq(0).type('B')
+      })
+
+    it.skip('Flight Booking',()=>{
+
+               // cy.get(".sc-eLwHnm.hHxEGr.fswWidgetPlaceholder").eq(0).type('B')
         // cy.wait(2000)
         // cy.contains("Mumbai, India (BOM)").click()
 
         cy.get(".sc-eLwHnm.hHxEGr.fswWidgetPlaceholder").eq(1).type('G')
         cy.wait(2000)
         cy.contains("Goa - Dabolim Airport").click()
+    })
+
+    it('Verify Label on the Home page',()=>{
+
+        cy.get('.sc-1gt8xf5-2.fbBmhE').should('have.text',"Domestic and International Flights")
+
+    })
+
+    it('Verify Trains link is visible',()=>{
+
+        cy.xpath('//div[@id="root"]//ul[@class="happy-nav"]/li[3]').should('have.text',"Trains")
+    })
+
+    it('Verify Hotels button',()=>{
+
+        cy.xpath('//div[@id="root"]//ul[@class="happy-nav"]/li[1]').click()
+        cy.get('.HomePagestyles__HomePageTitle-sc-s8m7jv-2.hEdjmN').should('have.text','Book Hotels & Homestays')
+        
     })
 })
