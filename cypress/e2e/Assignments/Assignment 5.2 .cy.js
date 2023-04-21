cfd5d6241af855b44a1e262a965f9fbf7c7642dc
Mahesh <mahesh.yambal@joshsoftware.com>
2023-04-21 11:11:14 +0530

    5.2 updated

--- cypress/e2e/Assignments/Assignment 5.2 .cy.js
+++ cypress/e2e/Assignments/Assignment 5.2 .cy.js
@@ -2,26 +2,15 @@ describe('Window Handling And Iframes',()=>{
   it('Tab Handling',()=>{
 
   cy.visit('https://www.globalsqa.com/demo-site/frames-and-windows')  //Parent Tab
+    //remove target element so that new page will open in same tab
   
-  //remove target element so that new page will open in same tab
-  cy.xpath('//div[@class="single_tab_div resp-tab-content resp-tab-content-active"]/a').invoke('removeAttr','target').click()   //clicking on link
+  cy.xpath('//div[@class="single_tab_div resp-tab-content resp-tab-content-active"]/a')
+        .invoke('removeAttr','target').click()   //clicking on link
   
-  cy.url().should('include','https://www.globalsqa.com/demo-site/frames-and-windows/#') 
+  cy.url().should('include','https://www.globalsqa.com/demo-site/') 
   
   cy.wait(2000)
 
    cy.go('back')   //Go back to parent tab
   })
-it.only('Handling Iframes',()=>{
-
-  cy.visit('https://www.globalsqa.com/demo-site/frames-and-windows')
-
-  const iframe = cy.get('#iFrame')
-  .its('0.contentDocument.body')
-  .should(be.visible)
-  .then(cy.wrap)
-  cy.get('//div[@class="logo_img"]').eq(2).should('have.text','GlobalSQA')
-
-})
-
 })
