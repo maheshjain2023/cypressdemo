f019c6a35ce537f6dd2b40e9903b6dcc66542dea
mahesh.yambal@gmail.com <mahesh.yambal@gmail.com>
2023-04-24 17:17:02 +0530

    6.3 submitted

--- cypress/e2e/Assignments/Assignment 6.3.cy.js
+++ cypress/e2e/Assignments/Assignment 6.3.cy.js
@@ -1,7 +1,11 @@
 describe('Assignment 6.3',()=>{
     it('File Upload',function(){
         cy.visit('https://ps.uci.edu/~franklin/doc/file_upload.html')
+        cy.contains('File Upload Test')
+        cy.contains('Send this file')
+        cy.get('[name="userfile"]').click()
         cy.get('[name="userfile"]').selectFile('../cypressdemo/cypress/fixtures/Test1.pdf')
+        //cy.contains('Test1.pdf')
         cy.get('[type="submit"]').click()
                   
     })
