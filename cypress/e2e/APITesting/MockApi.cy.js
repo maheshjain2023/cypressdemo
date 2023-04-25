f0e371640b079c7b50b311cedde0dd185b43a150
mahesh.yambal@gmail.com <mahesh.yambal@gmail.com>
2023-04-25 19:59:13 +0530

    Mock API updated

--- cypress/e2e/APITesting/MockApi.cy.js
+++ cypress/e2e/APITesting/MockApi.cy.js
@@ -2,18 +2,22 @@ describe('Create User API',()=>{
 
     it('Get Method',()=>{
 
-        cy.request({
-
-            method: 'GET',
-            url:'https://reqres.in//api/users?page=2'
+        // cy.request({
+        //     method: 'GET',
+        //     url:'https://reqres.in//api/users?page=2'
             
+        // })
+        cy.visit('https://reqres.in/')
+        cy.request('GET','https://reqres.in/api/users?page=2').then((response)=>{
+             cy.log(response.body.data[0].email)
         })
+        cy.intercept('GET','https://reqres.in/api/users?page=2',{statusCode : 201}
+        ).as('Waitfor')
+        cy.get('[data-id="users"]').click()
+        cy.wait('@Waitfor')
+        
 
 
-    })
-    // cy.intercept({
-    //     method:'POST',
-    //   https://bam.nr-data.net/events/1/ae504947bb?a=551253935&sa=1&v=1216.487a282&t=Unnamed%20Transaction&rst=13415&ck=1&ref=https://www.goibibo.com/
-
-    // })
+})
+    
 })
