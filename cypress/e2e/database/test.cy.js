b0ab5895a276155f5c611be08e7382a93217fb3a
mahesh.yambal@gmail.com <mahesh.yambal@gmail.com>
2023-04-26 18:43:13 +0530

    database update

--- cypress/e2e/database/test.cy.js
+++ cypress/e2e/database/test.cy.js
@@ -1,7 +1,17 @@
 describe("",()=>{
     it("test db",()=>{
-        cy.task("queryDb","select * from employee").then((res)=>{
-             cy.log(res)
+        cy.task("queryDb","select * from employee1").then((data)=>{
+            cy.log(data)
+            data.forEach((el)=>{
+                //cy.log(el)
+                cy.visit('http://www.webdriveruniversity.com/Contact-Us/contactus.html')
+                //cy.get('[name="first_name"]').type(el.firstname)
+                cy.xpath('//div[@class="row"]/form/input[1]').type(el.firstname)
+                cy.xpath('//div[@class="row"]/form/input[2]').type(el.lastname)
+                cy.xpath('//div[@class="row"]/form/input[3]').type(el.email)
+                cy.xpath('//div[@class="row"]/form/textarea').type(el.comments)
+            })
+             //cy.log(data)
         })
         //.then(count => {
         //     expect(count);
