2bac3469c4603b4edb87abe39b56bc035f966abf
mahesh.yambal@gmail.com <mahesh.yambal@gmail.com>
2023-04-28 15:29:18 +0530

    API update

--- cypress/e2e/APITesting/MockApi.cy.js
+++ cypress/e2e/APITesting/MockApi.cy.js
@@ -2,11 +2,10 @@ describe('Create User API',()=>{
 
     it('Get Method',()=>{
 
-        // cy.request({
-        //     method: 'GET',
-        //     url:'https://reqres.in//api/users?page=2'
-            
-        // })
+        //cy.request({
+        //      method: 'GET',
+        //      url:'https://reqres.in//api/users?page=2'
+        //  })
         cy.visit('https://reqres.in/')
         cy.request('GET','https://reqres.in/api/users?page=2').then((response)=>{
              cy.log(response.body.data[0].email)
